83ad13112fbdd632f90e022eeb469127c60fbf71
Your Name <you@example.com>
2024-09-21 08:44:43 -0400

    setup() refactored to seperate file

---
+++ examples/factory/setup.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "OneButton.h" /* https://github.com/mathertel/OneButton.git */
+#include "pin_config.h"
+
+extern OneButton button1;
+extern OneButton button2;
+
+void startSetup();
+
