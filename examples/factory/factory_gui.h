77c25b2f5be57a0db6d4347d244b233c3cfa575a
Your Name <you@example.com>
2024-09-21 07:34:03 -0400

    stripped all non-essential stuff like wifi, auto-time, animations

--- examples/factory/factory_gui.h
+++ examples/factory/factory_gui.h
@@ -2,7 +2,7 @@
 
 #define UI_BG_COLOR    lv_color_black()
 #define UI_FRAME_COLOR lv_color_hex(0x282828)
-#define UI_FONT_COLOR  lv_color_white()
+#define UI_FONT_COLOR  lv_color_hex(0x00DD00)
 #define UI_PAGE_COUNT  3
 
 #define MSG_NEW_HOUR   1
@@ -20,4 +20,3 @@
   } while (0);
 
 void ui_begin();
-void ui_switch_page(void);
